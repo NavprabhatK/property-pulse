124b6b5ce7c4a158876f32f1e867fb22ff6bfcaa
Navprabhat <navprabhatshashangan10@gmail.com>
2025-03-29 13:36:01 +0530

    property pulse complete project

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
+import FeaturedProperties from "@/components/FeaturedProperties";
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <>
+                <Hero/>
+                <InfoBoxes/>
+                <FeaturedProperties/>
+                <HomeProperties/>
+            </>
+
+        </div>);
+}
+
+export default HomePage;
